99afa936c901fb07db82f6046ab700e58db2d5cb
SUNG IL LEE <sungil.lee90@gmail.com>
2020-10-01 13:34:28 -0500

    re did tip amount calculator

--- FromJClass/TipAmount.py
+++ FromJClass/TipAmount.py
@@ -1,7 +1,13 @@
+# def tip_amount(tip_percentage, total):
+#     print("The total is: " + str(total))
+#     print("The tip percentage is: " + str(tip_percentage * 100) + "%")
+#     print("The tip amount will be : " + str(total * tip_percentage) + " dollars")
+#
+# tip_amount(.10,100)
+
 def tip_amount(tip_percentage, total):
     print("The total is: " + str(total))
-    print("The tip percentage is: " + str(tip_percentage * 100) + "%")
-    print("The tip amount will be : " + str(total * tip_percentage) + " dollars")
+    print("The tip percentage is: " + str(tip_percentage) + "%")
+    print("The tip amount will be : " + str(total * tip_percentage * 0.01) + " dollars")
 
-tip_amount(.10,100)
-
+tip_amount(10,100)
