a92ac50bd8a77f3985819ca8cc039dd090b2b00a
SUNG IL LEE <sungil.lee90@gmail.com>
2020-10-02 13:52:50 -0500

    added else if one elif

--- FromJClass/WatchMovie.py
+++ FromJClass/WatchMovie.py
@@ -2,13 +2,18 @@ def can_watch_movie(age, with_parents):
     if age >= 17:
         print("Welcome! Enjoy the show!")
         return True
+    # else:
+    #     if with_parents:
+    #         print("Yes, you can watch it!")
+    #         return True
+    #     else:
+    #         print("Sorry, you MUST to have your parents")
+    #         return False
+    elif age < 17 and with_parents:
+        print("Yes, you can watch it!")
+        return True
     else:
-        if with_parents:
-            print("Yes, you can watch it!")
-            return True
-        else:
-            print("Sorry, you MUST to have your parents")
-            return False
+        print("Sorry, you MUST to have your parents")
+        return False
 
-
-can_watch_movie(18, False)
+can_watch_movie(2, False)
