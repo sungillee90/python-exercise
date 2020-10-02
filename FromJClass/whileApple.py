a01f4ff9df01519d5763f78ae7c8ca2f56edae28
SUNG IL LEE <sungil.lee90@gmail.com>
2020-10-02 14:11:34 -0500

    added condition(boolean) to the whileApple loop

--- FromJClass/whileApple.py
+++ FromJClass/whileApple.py
@@ -1,8 +1,8 @@
-def eat_apples(num_of_apples):
+def eat_apples(num_of_apples, on_diet):
     apples_remaining = num_of_apples
 
     # Loop
-    while apples_remaining > 0:
+    while apples_remaining > 0 and not on_diet:
         print("Remaining apple = " + str(apples_remaining))
         apples_remaining -= 1
         print("Thank you!")
@@ -19,4 +19,4 @@ def eat_apples(num_of_apples):
 
 
 
-eat_apples(5)
+eat_apples(5, False)
