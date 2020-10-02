8bd22c424ded6cd5f64ff48d0876f471a6af660d
SUNG IL LEE <sungil.lee90@gmail.com>
2020-10-02 14:10:08 -0500

    while loop

---
+++ FromJClass/whileApple.py
@@ -0,0 +1,22 @@
+def eat_apples(num_of_apples):
+    apples_remaining = num_of_apples
+
+    # Loop
+    while apples_remaining > 0:
+        print("Remaining apple = " + str(apples_remaining))
+        apples_remaining -= 1
+        print("Thank you!")
+
+    print("Done")
+
+    return
+
+    # eat an apple
+    apples_remaining = apples_remaining - 1
+
+    # say thank you
+    # print("Thank you!")
+
+
+
+eat_apples(5)
