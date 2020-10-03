37de58f259f982dcb6738d46cf84cdd997a40e35
SUNG IL LEE <sungil.lee90@gmail.com>
2020-10-03 18:12:02 -0500

    practice the lists characteristic

--- FromJClass/Lists.py
+++ FromJClass/Lists.py
@@ -2,5 +2,9 @@ def list_number():
     lucky_numbers = [1,3,5,7,9,10]
 
     print(lucky_numbers)
+    print(lucky_numbers[1]) #3
+    print(lucky_numbers[-1]) #10
+    print(lucky_numbers[-3]) #7
+    print(type(lucky_numbers))
 
 list_number()
