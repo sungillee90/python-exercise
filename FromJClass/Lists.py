3198aaed9e6da93ba0c391302435a1a0199ade73
SUNG IL LEE <sungil.lee90@gmail.com>
2020-10-03 18:31:49 -0500

    used while loop to make it dynamic

--- FromJClass/Lists.py
+++ FromJClass/Lists.py
@@ -1,12 +1,31 @@
-def list_number():
-    lucky_numbers = [1,3,5,7,9,10]
+# def list_number():
+#     lucky_numbers = [1,3,5,7,9,10]
+#
+#     print(lucky_numbers)
+#     print(lucky_numbers[1]) #3
+#     print(lucky_numbers[-1]) #10
+#     print(lucky_numbers[-3]) #7
+#     print(type(lucky_numbers))
+#     print(lucky_numbers.append(15))
+#     print(lucky_numbers)
+#     print(lucky_numbers.pop(6))
+#     print(lucky_numbers)
+#
+# list_number()
 
-    print(lucky_numbers)
-    print(lucky_numbers[1]) #3
-    print(lucky_numbers[-1]) #10
-    print(lucky_numbers[-3]) #7
-    print(type(lucky_numbers))
-    print(lucky_numbers.append(15))
-    print(lucky_numbers)
+# P1: Write a fuction that prints each element in the list on a seperate line
+def print_list(lst):
+    i = 0
+    while i < len(lst):
+        print(lst[i])
+        i += 1
+    return
+"""
+print_list([1,2,3])
+1
+2
+3
+"""
+print_list([1,2,4,5,6165])
 
-list_number()
+#P2: Write a function outputs a list of numbers from 0 to n
