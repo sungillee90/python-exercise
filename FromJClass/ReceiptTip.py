f39a3859d6be1828abb631d758eda35a555c9f22
SUNG IL LEE <sungil.lee90@gmail.com>
2020-10-06 10:06:25 -0500

    refactor to better code

--- FromJClass/ReceiptTip.py
+++ FromJClass/ReceiptTip.py
@@ -3,10 +3,11 @@ def receive_tips(tips):
     for tip in tips:
         total_tips += tip
         # if tip amount is less than 5 dollar, do not say thank you.
-        if tip < 5:
-            continue
-        print("Thank you for tipping me $" + str(tip))
-
+        # if tip < 5:
+        #     continue
+        # print("Thank you for tipping me $" + str(tip))
+        if tip >= 5:
+            print("Thank you for tipping me $" + str(tip))
     return total_tips
 
 receive_tips([10, 5, 2])
