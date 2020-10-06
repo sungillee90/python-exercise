17c6cbccd480faa6b264ea078eea9d1b233b6603
SUNG IL LEE <sungil.lee90@gmail.com>
2020-10-06 10:05:05 -0500

    added continue in the for loop

--- FromJClass/ReceiptTip.py
+++ FromJClass/ReceiptTip.py
@@ -1,2 +1,12 @@
 def receive_tips(tips):
-    return 0
+    total_tips = 0
+    for tip in tips:
+        total_tips += tip
+        # if tip amount is less than 5 dollar, do not say thank you.
+        if tip < 5:
+            continue
+        print("Thank you for tipping me $" + str(tip))
+
+    return total_tips
+
+receive_tips([10, 5, 2])
