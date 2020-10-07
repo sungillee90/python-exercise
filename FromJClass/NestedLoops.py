6e456f7d8d196f2de2fbf0efebf3e1f9e90f3828
SUNG IL LEE <sungil.lee90@gmail.com>
2020-10-07 14:48:21 -0500

    added pyramid exercise

--- FromJClass/NestedLoops.py
+++ FromJClass/NestedLoops.py
@@ -4,3 +4,13 @@ def nested_loop():
             print(str(i) + str(j))
 
 nested_loop()
+
+def print_pyramid(n):
+    for i in range(n):
+        line_to_print = ""
+        for j in range(i):
+            line_to_print += "o"
+        print(line_to_print)
+    return
+
+print_pyramid(5)
