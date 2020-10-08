f78fedb439ca3d5488a331df3520691fd891cfe8
SUNG IL LEE <sungil.lee90@gmail.com>
2020-10-08 14:57:11 -0500

    added different way to make matrix

--- FromJClass/MatrixPractice.py
+++ FromJClass/MatrixPractice.py
@@ -1,2 +1,14 @@
+r1 = [1, 2, 3]
+r2 = [4, 5, 6]
+r3 = [7, 8, 9]
+
+matrix = [r1, r2, r3]
+
+matrix_two = [[1, 2, 3],
+              [1, 2, 4],
+              [4, 5, 8]]
+
+print(matrix)
+print(matrix_two)
 def print_matrix(m):
     return
