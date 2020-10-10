e021a707c6958d6872a1a0ddee6e4ce96b75a29c
SUNG IL LEE <sungil.lee90@gmail.com>
2020-10-09 23:20:49 -0500

    redo the matrix practice

--- FromJClass/MatrixPractice.py
+++ FromJClass/MatrixPractice.py
@@ -1,31 +1,40 @@
-r1 = [1, 2, 3]
-r2 = [4, 5, 6]
-r3 = [7, 8, 9]
+# r1 = [1, 2, 3]
+# r2 = [4, 5, 6]
+# r3 = [7, 8, 9]
+#
+# matrix = [r1, r2, r3]
+#
+# matrix_two = [[1, 2, 3, 5],
+#               [1, 2, 4, 6],
+#               [4, 5, 8, 7]]
+#
+# print(matrix)
+# print(matrix_two)
+# #  matrix [Row][Column] [0] [1] [2]
+# print(matrix_two[2][1])
+#
+# # print all of the single element
+# def print_matrix(m):
+#     if m == []:
+#         return
+#     num_rows = len(m)
+#     num_cols = len(m[0])
+#
+#     for row in m:
+#         for num in row:
+#             print(num)
+#
+#     print(num_rows)
+#     print(num_cols)
+#
+#
+# print_matrix(matrix_two)
 
-matrix = [r1, r2, r3]
+def creat_matrix(m,n):
+    my_matrix = []
+    for i in range(m):
+        my_matrix.append([0,0,0,0])
+    print(my_matrix)
+    return my_matrix
 
-matrix_two = [[1, 2, 3, 5],
-              [1, 2, 4, 6],
-              [4, 5, 8, 7]]
-
-print(matrix)
-print(matrix_two)
-#  matrix [Row][Column] [0] [1] [2]
-print(matrix_two[2][1])
-
-# print all of the single element
-def print_matrix(m):
-    if m == []:
-        return
-    num_rows = len(m)
-    num_cols = len(m[0])
-
-    for row in m:
-        for num in row:
-            print(num)
-
-    print(num_rows)
-    print(num_cols)
-
-
-print_matrix(matrix_two)
+creat_matrix(3,4)
