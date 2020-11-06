8fb786173cde04c08ff4a7e28310cd9413631dc1
SUNG IL LEE <sungil.lee90@gmail.com>
2020-11-06 15:14:09 -0600

    refactor to better soln

--- FromJClass/RecursionPrac.py
+++ FromJClass/RecursionPrac.py
@@ -1,8 +1,13 @@
 def func_R(n):
+    print("Start: " + str(n))
+    res = 0
     if n == 0:
-        return 0
+        res = 0
     else:
-        return func_R(n-1)+1
+        res = func_R(n-1)+1
+    print("End: " + str(n))
+    return res
 
 
+print(func_R(0))
 print(func_R(3))
