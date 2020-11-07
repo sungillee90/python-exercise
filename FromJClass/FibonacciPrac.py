6c72e7b251cdea3ae4f0a1ea9a5c4d42fa1e541b
SUNG IL LEE <sungil.lee90@gmail.com>
2020-11-07 01:05:40 -0600

    Completed Recursive Fibonacci problem

--- FromJClass/FibonacciPrac.py
+++ FromJClass/FibonacciPrac.py
@@ -1,3 +1,13 @@
 # Write Fibonacci function
 
 # f(0) = 0 ; f(1) = 1; f(n) = f(n-1) + f(n-2);
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+print(fib(5))
