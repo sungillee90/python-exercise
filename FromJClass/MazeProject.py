39d33ff852b31c99ceb8996c2434a34e75ecaf0b
SUNG IL LEE <sungil.lee90@gmail.com>
2020-12-27 19:32:07 -0600

    init setup for maze game

---
+++ FromJClass/MazeProject.py
@@ -0,0 +1,12 @@
+# Backtracking
+
+maze = [[".", "." "." "."],
+        [".", "x", "x", "x"],
+        [".", ".", ".", "x"],
+        ["x", "x", ".", "."]]
+
+def print_maze(maze):
+    return
+
+def solve_maze(maze):
+    return
