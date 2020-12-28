915b3db2253ef23fb770970b4a5c47af277363ab
SUNG IL LEE <sungil.lee90@gmail.com>
2020-12-27 19:36:52 -0600

    added maze setup

--- FromJClass/MazeProject.py
+++ FromJClass/MazeProject.py
@@ -6,7 +6,13 @@ maze = [[".", ".", ".", "."],
         ["X", "X", ".", "."]]
 
 def print_maze(maze):
-    return
+    for row in maze:
+        row_print = ""
+        for value in row:
+            row_print += value + " "
+        print(row_print)
 
+print(maze)
+print_maze(maze)
 def solve_maze(maze):
     return
