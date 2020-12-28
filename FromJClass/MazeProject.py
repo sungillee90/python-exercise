0fd568881c659ffe575edf10ad1a6072c0230c1d
SUNG IL LEE <sungil.lee90@gmail.com>
2020-12-28 09:09:31 -0600

    Added out of boundary situation

--- FromJClass/MazeProject.py
+++ FromJClass/MazeProject.py
@@ -32,3 +32,7 @@ def solve_maze_helper(maze, sol, pos_row, pos_col):
     # list starts 0, num of row 1 2 3 4
     if pos_row == num_row - 1 and pos_col == num_col - 1:
         return sol
+
+    # Out of bounds
+    if pos_row >= num_row or pos_col >= num_col:
+        return None
